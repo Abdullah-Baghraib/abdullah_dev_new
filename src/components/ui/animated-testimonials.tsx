a5b506c97653b4f46fe58db7797381442a6900bd
Abdullah-Baghraib <141523908+Abdullah-Baghraib@users.noreply.github.com>
2025-08-21 14:11:38 +0300

    Update animated-testimonials.tsx

--- src/components/ui/animated-testimonials.tsx
+++ src/components/ui/animated-testimonials.tsx
@@ -39,7 +39,7 @@ export const AnimatedTestimonials = ({
 
   useEffect(() => {
     if (autoplay) {
-      const interval = setInterval(handleNext, 5000);
+      const interval = setInterval(handleNext, 9000);
       return () => clearInterval(interval);
     }
   }, [autoplay]);
